f418c3a343f13aea4c974586ae44442774e85fe3
Melvin Strobl <melvin.strobl@web.de>
2019-11-29 15:31:45 +0100

    ui update and try to fix automation for this

--- src/main/python/unote/updatUI.py
+++ src/main/python/unote/updatUI.py
@@ -1,6 +1,6 @@
 from os import popen
 
-popen('python -m PyQt5.uic.pyuic .\\ui\\unote_gui.ui -o .\\ui\\unote_qt_export.py -x')
-popen('python -m PyQt5.uic.pyuic .\\ui\\preferences_gui.ui -o .\\ui\\preferences_qt_export.py -x')
-popen('pyrcc5.exe .\\style\\BreezeStyleSheets\\breeze.qrc -o .\\style\\BreezeStyleSheets\\breeze_resources.py')
-popen('pyrcc5.exe  .\\assets.qrc -o .\\assets.py')
+popen('py -m PyQt5.uic.pyuic .\\ui\\unote_gui.ui -o .\\ui\\unote_qt_export.py -x')
+popen('py -m PyQt5.uic.pyuic .\\ui\\preferences_gui.ui -o .\\ui\\preferences_qt_export.py -x')
+popen('py -m PyQt5.pyrcc.pyrcc_main .\\style\\BreezeStyleSheets\\breeze.qrc -o .\\style\\BreezeStyleSheets\\breeze_resources.py')
+popen('py -m PyQt5.pyrcc.pyrcc_main  .\\assets.qrc -o .\\assets.py')
