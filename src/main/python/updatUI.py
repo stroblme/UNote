d9cdea05c81821e4a484d83326f2c70a2459bd84
Melvin Strobl <melvin.strobl@web.de>
2019-12-15 14:04:53 +0100

    improved dark template

--- src/main/python/updatUI.py
+++ src/main/python/updatUI.py
@@ -4,7 +4,9 @@ import subprocess
 popen('py -m PyQt5.uic.pyuic .\\ui\\unote_gui.ui -o .\\unote_qt_export.py -x')
 popen('py -m PyQt5.uic.pyuic .\\ui\\preferences_gui.ui -o .\\preferences_qt_export.py -x')
 
-# subprocess.Popen([r"X:\\SSG - Style Sheet Generator\\src\\dist\\ssg\\ssg.exe", "-t", r"X:\\SSG - Style Sheet Generator\\src\\dist\\ssg\\resources\\dark.template", "-p","X:\\UNote\\src\\main\\python\\style\\palette.txt", "-o", "X:\\UNote\\src\\main\\python\\style\\BreezeStyleSheets\\dark.qss"])
+subprocess.Popen([r"X:\\SSG - Style Sheet Generator\\src\\dist\\ssg\\ssg.exe", "-t", r"X:\\SSG - Style Sheet Generator\\src\\dist\\ssg\\resources\\dark.template", "-p","X:\\UNote\\src\\main\\python\\style\\palette.txt", "-o", "X:\\UNote\\src\\main\\python\\style\\BreezeStyleSheets\\dark.qss"])
+
+subprocess.Popen([r"X:\\SSG - Style Sheet Generator\\src\\dist\\ssg\\ssg.exe", "-t", r"X:\\SSG - Style Sheet Generator\\src\\dist\\ssg\\resources\\light.template", "-p","X:\\UNote\\src\\main\\python\\style\\palette.txt", "-o", "X:\\UNote\\src\\main\\python\\style\\BreezeStyleSheets\\light.qss"])
 
 popen('pyrcc5 .\\style\\BreezeStyleSheets\\breeze.qrc -o .\\style\\BreezeStyleSheets\\breeze_resources.py')
 popen('pyrcc5  .\\assets.qrc -o .\\assets.py')
